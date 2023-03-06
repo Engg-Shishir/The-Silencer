0e8847b1d839f4fcc68ea37c0ea55d7acd641f6f
Shishir Bhuiyan <shishirbhuiyan83@gmail.com>
2023-03-07 00:32:29 +0600

    4 March 2023

---
+++ MySql/Leetcode/Recyclable and Low Fat Products .sql
@@ -0,0 +1,61 @@
+/*
+    Problem Name : Recyclable and Low Fat Products
+    Source       : https://leetcode.com/problems/recyclable-and-low-fat-products
+    Author       : Shishir Bhuiyan
+    Leetcode     : engg-shishir
+    Date         : 04/March/2023
+*/
+
+select product_id from Products where low_fats='Y' and recyclable='Y';
+
+
+
+
+/*
+
+Table: Products
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| product_id  | int     |
+| low_fats    | enum    |
+| recyclable  | enum    |
++-------------+---------+
+product_id is the primary key for this table.
+low_fats is an ENUM of type ('Y', 'N') where 'Y' means this product is low fat and 'N' means it is not.
+recyclable is an ENUM of types ('Y', 'N') where 'Y' means this product is recyclable and 'N' means it is not.
+ 
+
+Write an SQL query to find the ids of products that are both low fat and recyclable.
+
+Return the result table in any order.
+
+The query result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Products table:
++-------------+----------+------------+
+| product_id  | low_fats | recyclable |
++-------------+----------+------------+
+| 0           | Y        | N          |
+| 1           | Y        | Y          |
+| 2           | N        | Y          |
+| 3           | Y        | Y          |
+| 4           | N        | N          |
++-------------+----------+------------+
+Output: 
++-------------+
+| product_id  |
++-------------+
+| 1           |
+| 3           |
++-------------+
+Explanation: Only products 1 and 3 are both low fat and recyclable.
+
+
+*/
